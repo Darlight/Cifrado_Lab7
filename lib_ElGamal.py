1d65fd47ee827f5ac89df43a8a0badfb60cf0031
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-10-04 16:48:11 -0600

    Update lib_ElGamal.py

--- lib_ElGamal.py
+++ lib_ElGamal.py
@@ -5,23 +5,28 @@ CC3078 - Cifrado de informacion
 Grupo #4
 lib_ElGamal.py
 Descripcion: Algoritmo ElGamal con libreria. 
-link del codigo: https://pythonhosted.org/pycrypto/Crypto.PublicKey.ElGamal-module.html
+Codigo obtenido de: https://pypi.org/project/elgamal/#description
 """
-from Crypto import Random
-from Crypto.Random import random
-from Crypto.PublicKey import ElGamal
-from Crypto.Util.number import GCD
-from Crypto.Hash import SHA
+from elgamal.elgamal import Elgamal 
 
-message = "Hello"
-key = ElGamal.generate(1024, Random.new().read)
-h = SHA.new(message).digest()
-while 1:
-    k = random.StrongRandom().randint(1,key.p-1)
-    if GCD(k,key.p-1)==1: break
-sig = key.sign(h,k)
+# Mensaje a cifrar
+m = b'Josue'
+print("Mensaje a cifrar:",m)
 
-if key.verify(h,sig):
-    print("OK")
-else:
-    print("Incorrect signature")
+# Se generar las llaves
+# pb = llave publica
+# pv = llave privada
+# En este caso ambas llaves son de 128 bits
+pb, pv = Elgamal.newkeys(128)
+print("\nNumero primo: ", pb.p)
+print("\nGenerador: ", pb.g)
+print("\nLlave publica: ", pb)
+print("\nLlave privada: ", pv)
+
+# Se cifra el mensaje utilizando la llave publica
+ct = Elgamal.encrypt(m, pb)
+print("\nTexto cifrado: c", t)
+
+# Se descifra el mensaje cifrado utilizando la llave privada
+dd = Elgamal.decrypt(ct, pv)
+print("\nTexto descifrado: ", dd)
